8dd6e4ca6d4d8608203e489154df92ce5f4638d4
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-21 18:28:40 -0700

    Create FlickrAPI class

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
+import retrofit2.Retrofit
+import retrofit2.create
 
 class PhotoGalleryFragment: Fragment() {
     private var _binding: FragmentPhotoGalleryBinding? = null
@@ -24,6 +27,16 @@ class PhotoGalleryFragment: Fragment() {
             FragmentPhotoGalleryBinding.inflate(inflater, container, false)
         binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://api.flickr.com/")
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create<FlickrApi>()
     }
 
     override fun onDestroyView() {
