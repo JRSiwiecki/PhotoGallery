33b7ff734b5f53de4b981e68fc224640f0fba36e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-21 19:41:02 -0700

    Add wrapper classes

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
-import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -35,12 +34,16 @@ class PhotoGalleryFragment: Fragment() {
         return binding.root
     }
 
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            val response = PhotoRepository().fetchPhotos()
-            Log.d(TAG, "Response received: $response")
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                photoGalleryViewModel.galleryItems.collect { items ->
+                    Log.d(TAG, "Response received: $items")
+                }
+            }
         }
     }
 
