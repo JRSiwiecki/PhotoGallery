eb0ce69098519fbecde2356ca74e8da55a2560f4
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-22 22:39:35 -0700

    Remove api key from FLickrAPI class

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,15 +1,12 @@
 package com.bignerdranch.android.photogallery.api
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
-private const val API_KEY = "b0ef0a2eb5b96241f9af9e82d51ed43c"
 interface FlickrApi {
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=$API_KEY" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+    @GET("services/rest/?method=flickr.interestingness.getList")
     suspend fun fetchPhotos(): FlickrResponse
+
+    @GET("services/rest?method=flickr.photos.search")
+    suspend fun searchPhotos(@Query("text") query: String): FlickrResponse
 }
